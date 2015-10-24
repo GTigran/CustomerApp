f2d3ab7ca752e491107c2269e3d016ebf686d60b
Tigran Gyulzadyan <tigran.gyulzadyan@gmail.com>
2015-10-23 17:51:10 -0700

    Commiting changes to master

--- CustomerManagement.BusinessObjects/Models/IIdentityModel.cs
+++ CustomerManagement.BusinessObjects/Models/IIdentityModel.cs
@@ -5,6 +5,9 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace CustomerManagement.BusinessObjects.Models
 {
     public interface IIdentityModel
